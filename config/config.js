8acfe0e74e9bfe5cac4b3f1790a95688855e50a9
Mrityunjay <mrityunjay.mukherjee@ril.com>
2019-11-18 12:28:15 +0530

    update k8s yamls

--- config/config.js
+++ config/config.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 // get the NODE_ENV
-const NODE_ENV = process.env.NODE_ENV || 'development'
+const NODE_ENV = process.env.NODE_ENV || 'default'
 // get the values maintained in config json file
-let configBuffer = fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf-8')
+let configFilePath = 'data/config.json'
+if (NODE_ENV !== 'default') {
+    configFilePath = '/etc/config/config.json'
+}
+let configBuffer = fs.readFileSync(configFilePath, 'utf-8')
+// let configBuffer = fs.readFileSync(path.resolve(__dirname, 'data/config.json'), 'utf-8')
 let config = JSON.parse(configBuffer)
 // get the environment specific variables support for MONGO, Redis, Kafka
 // console.log(config)
